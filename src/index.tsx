3479d4c31fed8dd52d1ce3c8364d8710143ced13
Zijie Tan <ztan4@ualberta.ca>
2024-11-18 00:10:42 -0700

    initiating a react app

---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
+const container = document.getElementById("root");
+
+if (container) {
+    createRoot(container).render(
+        <React.StrictMode>
+            <BrowserRouter>
+            {/* <TODO></TODO> */}
+            </BrowserRouter>
+        </React.StrictMode>
+    );
+}
+
