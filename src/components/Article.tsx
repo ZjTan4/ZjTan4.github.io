bf8d40c43092e81454874ee43384b9471a632401
Zijie Tan <ztan4@ualberta.ca>
2024-12-04 16:15:03 -0700

    markdown rendering down

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 
 import "@assets/styles/layout.css"
 
@@ -25,7 +27,33 @@ const Article : React.FC<ArticleProps> = ({articleDetail}) => {
             <p>
                 <em>Published on: {new Date(articleDetail.createdAt).toLocaleDateString()}</em>
             </p>
-            
+            <ReactMarkdown
+                children={articleDetail.body}
+                remarkPlugins={[remarkGfm]}
+                components={{
+                    // Ensure Return Types Are React Elements
+                    a: ({ href, children }) => {
+                        if (!href) return <>{children}</>
+                        return (
+                            <a href={href} target="_blank" rel="noopener noreferrer">
+                                {children}
+                            </a>
+                        )
+                    },
+                    code: ({ inline, children, ...props}: React.ComponentProps<"code"> & {inline?:boolean}) => 
+                        inline ? (
+                            <code className="inline-code" {...props}>
+                                {children}
+                            </code>
+                        ) : (
+                            <pre className="block-code">
+                                <code {...props}>
+                                    {children}
+                                </code>
+                            </pre>
+                        )
+                }}
+            />
         </div>
     )
 };
